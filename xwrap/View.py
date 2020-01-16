14a7da45bd841ecd60ee08611ea3ac94ec561f44
Xett <3954937+Xett@users.noreply.github.com>
2020-01-17 02:56:07 +1000

    Revert "Add image variable to BufferBitmap"
    This reverts commit 7f6a5c138d6542dc2f504bd659f7775ef5dfdf4a.

--- xwrap/View.py
+++ xwrap/View.py
@@ -72,7 +72,6 @@ class BufferBitmap:
         self.parent=parent
         self.name=name
         self.events=self.parent.events
-        self.image=wx.MemoryDC()
         self.images={}
         self.events.AddEvent(BUFFER_BITMAP_DRAW_BUFFER_EVENT)
         self.events.AddEvent(BUFFER_BITMAP_ONSIZE_EVENT)
