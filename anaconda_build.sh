37d1c746e70beab57ea056959bf63e5e2ffe53ad
Xett <ethan.jones@my.jcu.edu.au>
2020-01-19 05:54:28 +1000

    Update anaconda_build.sh

--- anaconda_build.sh
+++ anaconda_build.sh
@@ -1,4 +1,3 @@
-setupName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
-conda build "./anaconda_build/$setupName"
-conda install --use-local -y --force-reinstall $setupName 
+conda build .
+conda install --use-local -y --force-reinstall $(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
 conda build purge
