b9ce849ee5743b428f43f3a6f9317ad63b06ed25
Xett <ethan.jones@my.jcu.edu.au>
2020-01-19 05:09:52 +1000

    Revert "Close processes at Close"
    This reverts commit 159e3cc2a2f2389041ec87d21de23f0483731019.

--- xwrap/Events.py
+++ xwrap/Events.py
@@ -103,5 +103,4 @@ class Events:
                 except:
                     continue
             output.put((mp.current_process().name,mp.current_process().pid,task))
-        mp.current_process().join()
     Worker=classmethod(Worker)
